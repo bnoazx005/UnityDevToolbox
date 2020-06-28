8ebcafb9c2dfad9132e0c7d793d4b9da9c9ab0c3
bnoazx005 <ildar2571@yandex.ru>
2020-06-28 17:31:39 +0400

    Add coroutine context implementation

---
+++ UnityDevToolbox/Common/CoroutineContext.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+
+namespace UnityDevToolbox.Impls
+{
+    public class CoroutineContext
+    {
+        private MonoBehaviour mContextOwner;
+
+        public CoroutineContext(MonoBehaviour contextOwner)
+        {
+            mContextOwner = contextOwner ?? throw new ArgumentNullException("contextOwner");
+        }
+
+        public Coroutine ExecuteCoroutine(IEnumerator coroutine)
+        {
+            return mContextOwner.StartCoroutine(coroutine);
+        }
+
+        public void StopCoroutine(Coroutine coroutine)
+        {
+            mContextOwner.StopCoroutine(coroutine);
+        }
+    }
+}
